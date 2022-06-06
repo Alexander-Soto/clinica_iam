1ec3839509226c0d867a6c451706ba241d303e9b
Alexander Antonio Soto Brito <84160429+Alexander-Soto@users.noreply.github.com>
2022-06-06 00:01:49 -0600

    Corrigiendo menu

--- vista/menu.php
+++ vista/menu.php
@@ -67,57 +67,6 @@ if ($_SESSION['usuario'] == '') {
             </div>
         </div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
-    <link rel="icon" href="../recursos/clinica.png">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="menu.css">
-    <title>Clinica IAM</title>
-</head>
-
-<body>
-    <div class="wrapper">
-        <nav id="sidebar">
-            <div class="sidebar-header">
-                <img src="../recursos/recepcion.png" style="width: 38px;">
-                <h3 style="margin-left: 10px; height: 32px; padding-top: 6px;">Menu</h3>
-            </div>
-            <ul class="list-unstyled components">
-                <li>
-                    <a href="#" onclick="CargarPacientes()"><img src="../recursos/registro.png"> Pacientes</a>
-                </li>
-                <li>
-                    <a href="#" onclick="CargarConsulta()"><img src="../recursos/consulta.png"> Consulta</a>
-                </li>
-                <li>
-                    <a href="#"><img src="../recursos/agenda.png"> Agenda</a>
-                </li>
-                <li>
-                    <a href="#"><img src="../recursos/factura.png"> Facturas</a>
-                </li>
-                <li>
-                    <a href="#"><img src="../recursos/vademecum.png"> Vademecum</a>
-                </li>
-                <li>
-                    <a href="#"><img src="../recursos/cie10.png"> CIE10</a>
-                </li>
-                <li>
-                    <a href="#"><img src="../recursos/servicios.png"> Servicios</a>
-                </li>
-            </ul>
-            <div style="padding: 20px;">
-                <a href="#" onclick="CerrarSesion()"><img src="../recursos/logout.png"> Cerrar Sesion</a>
-            </div>
-        </nav>
-        <div id="divContenido">
-        </div>
-    </div>
-
     <script type="text/javascript" src="menu.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
@@ -130,3 +79,6 @@ if ($_SESSION['usuario'] == '') {
 </body>
 
 </html>
+<?php 
+} 
+?>
